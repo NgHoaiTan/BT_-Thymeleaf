84fcd863765fed9224dd5b4b74ee438456e2bc3d
NguyenHoaiTan <tannh3108@gmail.com>
2024-10-28 08:05:43 +0700

    Khoi tao Project va taoj file index.html

---
+++ src/test/java/vn/hoaitan/BT_/Thymeleaf/BtThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hoaitan.BT_.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
